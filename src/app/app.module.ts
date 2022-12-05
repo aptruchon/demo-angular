87aa21270333d23304cf93f6f5d15d40f2fde4b8
aptruchon <aptruchon@gmail.com>
2022-12-05 13:02:30 -0500

    Ajout des composantes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EnteteComponent } from './entete/entete.component';
+import { ProduitComponent } from './produit/produit.component';
+import { ListeProduitComponent } from './liste-produit/liste-produit.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { NonTrouveComponent } from './non-trouve/non-trouve.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EnteteComponent,
+    ProduitComponent,
+    ListeProduitComponent,
+    AccueilComponent,
+    NonTrouveComponent
   ],
   imports: [
     BrowserModule,
