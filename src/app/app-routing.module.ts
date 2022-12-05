ae4c50d09c791d9389da0a0fe7da5cb1384aa51c
aptruchon <aptruchon@gmail.com>
2022-12-05 13:14:25 -0500

    Ajout de routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ListeProduitComponent } from './liste-produit/liste-produit.component';
+import { NonTrouveComponent } from './non-trouve/non-trouve.component';
+import { ProduitComponent } from './produit/produit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component:AccueilComponent},
+  {path: "produit", component:ListeProduitComponent},
+  {path: "**", component:NonTrouveComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
